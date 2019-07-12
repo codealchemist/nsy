ab5b7a78086e9ed927890ec1a12af1c58c9239a1
Alberto Miranda <alberto.miranda@minted.com>
2019-07-12 20:51:19 -0300

    Added in concert mode; added routing; added components and config.

---
+++ src/components/Animation/Animation.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+import config from 'config/index'
+
+const { logo } = config
+
+const Container = styled.div`
+  background: black;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+`
+
+const RotatingLogo = styled.div`
+  @keyframes rotate {
+    from {
+      transform: rotate(0deg);
+    }
+  
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  width: 100%;
+  height: 100%;
+  background-image: url(${logo});
+  background-repeat: no-repeat;
+  background-size: auto 85%;
+  background-position: center;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  animation: rotate 4s linear infinite;
+`
+
+export default class Animation extends React.PureComponent {
+  render () {
+    return (
+      <Container>
+        <RotatingLogo />
+      </Container>
+    )
+  }
+}
---
+++ src/components/Player/Player.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import AudioPlayer from 'react-jinke-music-player'
+import config from 'config/index'
+import 'react-jinke-music-player/assets/index.css'
+
+const { logo } = config
+
+export default class Player extends React.PureComponent {
+  state = {
+    options: {
+      audioLists: [
+        {
+          name: 'Come Together',
+          singer: 'NSY',
+          musicSrc:
+            'https://res.cloudinary.com/hnkdyabc4/video/upload/v1562265042/audio/02-come-together_rtlzff.mp3',
+          cover: logo
+        },
+        {
+          name: 'Eye of the Tiger',
+          singer: 'NSY',
+          musicSrc:
+            'https://res.cloudinary.com/hnkdyabc4/video/upload/v1562265025/audio/eye-of-the-tiger_laih7c.mp3',
+          cover: logo
+        },
+        {
+          name: 'Estrella',
+          singer: 'NSY',
+          musicSrc:
+            'https://res.cloudinary.com/hnkdyabc4/video/upload/v1562449054/audio/the-reef_t29ztu.mp3',
+          cover: logo
+        },
+        {
+          name: 'Another Brick in the Wall (part 2)',
+          singer: 'NSY',
+          musicSrc:
+            'https://res.cloudinary.com/hnkdyabc4/video/upload/v1562265045/audio/the-wall_kwlx2i.mp3',
+          cover: logo
+        },
+        {
+          name: 'Rehén',
+          singer: 'NSY',
+          musicSrc:
+            'https://res.cloudinary.com/hnkdyabc4/video/upload/v1562291630/audio/rehen_j2pyg8.mp3',
+          cover: logo
+        },
+        {
+          name: 'Under Pressure',
+          singer: 'NSY',
+          musicSrc:
+            'https://res.cloudinary.com/hnkdyabc4/video/upload/v1562265046/audio/under-pressure_av2y1j.mp3',
+          cover: logo
+        }
+      ],
+      theme: 'dark',
+      // remember: true,
+      remove: false,
+      // mode: 'full',
+      autoPlay: false,
+      showPlayMode: false,
+      showLyric: false,
+      preload: true,
+      showMiniProcessBar: true,
+      defaultPosition: {
+        top: 'calc(50% + 60px)',
+        left: 'calc(50% - 40px)'
+      }
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+          <AudioPlayer {...this.state.options} />
+        </div>
+    )
+  }
+}
